a2295bc4ac16c9dc85bc2fbc64d42556e6f98cc1
quantmind <quantmind@citi.com>
2010-09-20 17:43:28 +0100

    more docs on dataloader

--- dynts/data/__init__.py
+++ dynts/data/__init__.py
@@ -2,6 +2,8 @@ from datetime import date, timedelta
 
 from dynts.conf import settings
 
+from ccy import todate
+
 from gy import DataProvider, google, yahoo
 
 
@@ -10,22 +12,23 @@ class MissingDataProvider(Exception):
     pass
 
 
+def safetodate(dte):
+    try:
+        return todate(end)
+    except:
+        return None
+
+
 class TimeSerieLoader(object):
     '''Load timeseries. This class can be replaced by a custom one.'''
     
-    def dates(self, start, end):
-        end   = end or date.today()
-        start = start
-        if not start:
-            start = end - timedelta(days=int(round(30.4*settings.months_history)))
-        return start,end
-            
-    def parse_symbol(self, symbol, provider =None):
-        provider = provider or settings.default_provider
-        return str(symbol), None, provider
+    def load(self, providers, symbols, start, end, provider = None):
+        '''Load symbols data.
         
-    def __call__(self, providers, symbols, start, end, provider = None):
-        '''Load data symbols'''
+* *providers* Dictionary of registered data providers.
+* *symbols* list of symbols to load
+* *start* start date
+* *end* end date'''
         start, end = self.dates(start, end)
         data = {}
         for symbol in symbols:
@@ -36,6 +39,24 @@ class TimeSerieLoader(object):
             ts = p.get(ticker, start, end, field) 
             data[symbol] = ts
         return data
+    
+    def dates(self, start, end):
+        '''Preconditioning on dates. This function makes sure the *start*
+and *end* date are consistent. It never fails and always return a two-element tuple
+containing *start*,*end* with *start* less or equal *end*
+and *end* never after today.
+There should be no reason to override this function.'''
+        td    = date.today()
+        end   = safetodate(end) or td
+        end   = end if end <= td else td
+        start = safetodate(start)
+        if not start or start > end:
+            start = end - timedelta(days=int(round(30.4*settings.months_history)))
+        return start,end
+    
+    def parse_symbol(self, symbol, provider =None):
+        provider = provider or settings.default_provider
+        return str(symbol), None, provider
 
 
 class DataProviders(dict):
@@ -45,7 +66,7 @@ class DataProviders(dict):
         loader = loader or TimeSerieLoader()
         if isinstance(loader,type):
             loader = loader()
-        return loader(self,symbols,start,end, provider = provider)
+        return loader.load(self,symbols,start,end, provider = provider)
     
     def register(self, provider):
         '''Register a new data provider. *provider* must be an instance of
