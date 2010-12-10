a42244aab9db66124703f5100c70442927368c46
lsbardel <luca.sbardella@gmail.com>
2010-12-10 08:00:44 +0000

    added 2 profile files

---
+++ profile/zoorollmin.py
@@ -0,0 +1,11 @@
+from baseprofile import *
+
+ts = randomts(10000, backend = 'zoo')
+
+cProfile.runctx("ts.rollmin(window=100)",
+                globals(),
+                locals(),
+                "Profile.prof")
+
+s = pstats.Stats("Profile.prof")
+s.strip_dirs().sort_stats("time").print_stats()
