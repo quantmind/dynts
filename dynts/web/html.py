7d76dcf6c1d569558d5b44bdfbea05722626f4ae
quantmind <luca.sbardella@citi.com>
2011-12-23 09:53:03 +0000

    web fix

--- dynts/web/html.py
+++ dynts/web/html.py
@@ -1,13 +1,22 @@
 from djpcms import html
 from djpcms.utils import media
 
-SPARKLINE_MEDIA = media.Media(js = ['dynts/jquery.sparkline.js',
-                                    'dynts/decorator.js'])
+SPARKLINE_MEDIA = ['dynts/jquery.sparkline.js',
+                   'dynts/decorator.js']
+
+FLOT_MEDIA = media.Media(js = ['dynts/flot/excanvas.min.js',
+                              'dynts/flot/jquery.flot.js',
+                              'dynts/flot/jquery.flot.selection.js',
+                              'dynts/jquery.flot.text.js',
+                              'dynts/base64.js',
+                              'dynts/canvas2image.js',
+                              'dynts/ecoplot/ecoplot.js']+\
+                              SPARKLINE_MEDIA)
 
 sparkline_maker = html.WidgetMaker(
                     tag = 'span',
                     default_class = 'sparkline',
-                    media = SPARKLINE_MEDIA)
+                    media = FLOT_MEDIA)
 
 def sparkline(data, **options):
     return  html.Widget(sparkline_maker)\
